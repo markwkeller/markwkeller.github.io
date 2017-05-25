4298747aad50fca7418c88a2b4bf36d41f945848
Mark Keller <mwkeller@umich.edu>
2017-05-24 21:44:45 -0700

    stuff

--- js/dots.js
+++ js/dots.js
@@ -1,173 +1,156 @@
 var winHeight = 400;
 var winWidth = 400;
-var playerArray = [0, 0, 0, 0, 0, 0];
+var playerArray = [0,0,0,0,0,0];
 var turn = 0;
 var nobody = false;
-var count = [0, 0, 0, 0, 0, 0];
+var count = [0,0,0,0,0,0];
 var gridSize = 31;
 var rowCount = 0;
 var columnCount = 0;
-var whitelist = [[], []];
-var brownlist = [[], []];
+var whitelist = [[],[]];
+var brownlist = [[],[]];
 
-function addLoadEvent(func) {
-    "use strict";
-    var oldonload = window.onload;
-    if (typeof window.onload !== 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function () {
-            if (oldonload) {
-                oldonload();
-            }
-            func();
-        };
-    }
+function startup1(e){
+	winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	scaleGrid();
+	playerChange("p0");
+	playerChange("p1");
 }
 
-function checkLast() {
-    "use strict";
-	var gridCount = columnCount * rowCount, pieceCount = 0, j, i, countClass = document.getElementsByClassName("count");
-	for (j = 0; j < 6; j += 1) {
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
+addLoadEvent(startup1);
+
+window.onresize = function(event) {
+	winWidth = Math.max(window.innerWidth, document.documentElement.clientWidth, document.body.clientWidth);
+	winHeight = Math.max(window.innerHeight, document.documentElement.clientHeight, document.body.clientHeight);
+	scaleGrid();
+	resetCount();
+};
+
+window.onkeypress=function(){
+	var countClass =	document.getElementsByClassName("count");
+	for(i=0;i<countClass.length;i++){
+		if(countClass[i].style.display == "block"){
+			countClass[i].style.display = "none";
+		}
+		else{
+			countClass[i].style.display = "block";
+		}
+	}
+};
+
+function placePiece(id){
+	var name = "dot" + turn;
+	if(document.getElementById(id+"div").className == "dotEmpty"){
+		document.getElementById(id+"div").className = name;
+		count[turn]= count[turn]+1;
+		document.getElementById("p"+turn+"count").innerHTML = count[turn];
+		checkTaken(id);
+		checkCapture(id);
+		checkLast();
+		takeTurn();
+	}
+}
+
+function checkLast(){
+	var gridCount = columnCount*rowCount;
+	var pieceCount = 0;
+	for(j=0;j<6;j++){
 		pieceCount = pieceCount + count[j];
 	}
-	if (pieceCount >= gridCount) {
-		for (i = 0; i < countClass.length; i += 1) {
+	if(pieceCount>=gridCount){
+		var countClass =	document.getElementsByClassName("count");
+		for(i=0;i<countClass.length;i++){
 			countClass[i].style.display = "block";
 		}
 	}
 }
-function white(xnew, ynew) {
-    "use strict";
-	var whiteb = false, i;
-	for (i = 0; i < whitelist[0].length; i += 1) {
-		if (whitelist[0][i] === xnew && whitelist[1][i] === ynew) {
-			whiteb = true;
-		}
-	}
-	if (whiteb === false) {
-		whitelist[0].push(xnew);
-		whitelist[1].push(ynew);
-	}
-	return whiteb;
-}
-function brown(xnew, ynew) {
-    "use strict";
-	var brownb = false, i;
-	for (i = 0; i < brownlist[0].length; i += 1) {
-		if (brownlist[0][i] === xnew && brownlist[1][i] === ynew) {
-			brownb = true;
-		}
-	}
-	if (brownb === false) {
-		brownlist[0].push(xnew);
-		brownlist[1].push(ynew);
-	}
-	return brownb;
-}
-function checkAdjacent(x, y, cl) {
-    "use strict";
-	var edge = false, adjacents = [0, 0, 0, 0], xnew, ynew, left, right, top, bottom;
-	if (x > 0) {
-		xnew = x - 1;
-		ynew = y;
-		left = xnew.toString() + "," + ynew.toString();
-		if (document.getElementById(left + "div").className !== cl) {
-			if (white(xnew, ynew) === false) {
-				adjacents[1] = 1;
-			}
-			if (document.getElementById(left + "div").className !== "dotEmpty") {
-				brown(xnew, ynew);
-			}
-		}
-	} else {edge = true; }
-	if (x < columnCount - 1) {
-		xnew = x + 1;
-		ynew = y;
-		right = xnew.toString() + "," + ynew.toString();
-		if (document.getElementById(right + "div").className !== cl) {
-			if (white(xnew, ynew) === false) {
-				adjacents[2] = 1;
-			}
-			if (document.getElementById(right + "div").className !== "dotEmpty") {
-				brown(xnew, ynew);
-			}
-		}
-	} else {edge = true; }
-	if (y > 0) {
-		xnew = x;
-		ynew = y - 1;
-		top = xnew.toString() + "," + ynew.toString();
-		if (document.getElementById(top + "div").className !== cl) {
-			if (white(xnew, ynew) === false) {
-				adjacents[0] = 1;
-			}
-			if (document.getElementById(top + "div").className !== "dotEmpty") {
-				brown(xnew, ynew);
-			}
-		}
-	} else {edge = true; }
-	if (y < rowCount - 1) {
-		xnew = x;
-		ynew = y + 1;
-		bottom = xnew.toString() + "," + ynew.toString();
-		if (document.getElementById(bottom + "div").className !== cl) {
-			if (white(xnew, ynew) === false) {
-				adjacents[3] = 1;
-			}
-			if (document.getElementById(bottom + "div").className !== "dotEmpty") {
-				brown(xnew, ynew);
-			}
-		}
-	} else {edge = true; }
-	return edge;
-}
-function resetCount() {
-    "use strict";
-	count = [0, 0, 0, 0, 0, 0];
-	var tds = document.getElementsByTagName("td"), tdCount = 0, i, j, tdid, cl, piece, turn;
-	for (i = 0; i < rowCount; i += 1) {
-		for (j = 0; j < columnCount; j += 1) {
-			tdid = tds[tdCount].id;
-			cl = document.getElementById(tdid + "div").className;
-			piece = cl.split("dot")[1];
-			if (piece !== "Empty") {
-				turn = parseInt(piece, 10);
-				count[turn] = count[turn] + 1;
-			}
-			tdCount += 1;
-		}
-	}
-	for (i = 0; i < 6; i += 1) {
-		document.getElementById("p" + i + "count").innerHTML = count[i];
-	}
-}
-function checkTaken(id) {
-    "use strict";
-	var idString = id.split(","), x = parseInt(idString[0], 10), y = parseInt(idString[1], 10), cl = document.getElementById(id + "div").className, clSplit = cl.split("t"), clInt = parseInt(clSplit[1], 10), g, k, oldLength = 0, newLength = 0, edge = false;
-	for (g = 0; g < 6; g += 1) {
-		if (g !== clInt) {
-			whitelist = [[x], [y]];
-			do {
+
+function checkTaken(id){
+	var idString = id.split(",");
+	var x = parseInt(idString[0]);
+	var y = parseInt(idString[1]);
+	var cl = document.getElementById(id+"div").className;
+	var clSplit = cl.split("t");
+	var clInt = parseInt(clSplit[1]);
+	for(g=0; g<6; g++){
+		if(g!=clInt){
+			whitelist = [[x],[y]];
+			var oldLength = 0;
+			var newLength = 0;
+			var edge = false;
+			do{
 				oldLength = whitelist[0].length;
-				for (k = 0; k < oldLength; k += 1) {
-					if (checkAdjacent(whitelist[0][k], whitelist[1][k], "dot" + g.toString()) === true) {
-						edge = true;
+				for(k=0; k<oldLength; k++){
+					if(checkAdjacent(whitelist[0][k],whitelist[1][k],"dot"+g.toString())==true){
+						edge=true;
 						break;
 					}
 				}
-				if (edge === true) {break; }
-				newLength = whitelist[0].length;
-			} while (newLength > oldLength);
-			if (edge === false) {
-				document.getElementById(id + "div").className = "dot" + g.toString();
+				if(edge==true){break;}
+				newLength=whitelist[0].length;
+			}while(newLength>oldLength);
+			if(edge==false){
+				document.getElementById(id+"div").className = "dot"+g.toString();
 				resetCount();
 				return;
 			}
 		}
 	}
 }
-function checkStart(x, y, cl) {
+
+function checkCapture(id){
+	var idString = id.split(",");
+	var x = parseInt(idString[0]);
+	var y = parseInt(idString[1]);
+	blacklist = [[],[]];
+	var cl = document.getElementById(id+"div").className;
+	checkStart(x,y,cl);
+	for(h=0;h<blacklist[0].length;h++){
+		whitelist = [[blacklist[0][h]],[blacklist[1][h]]];
+		brownlist = [[],[]];
+		if(document.getElementById(blacklist[0][h].toString() + "," + blacklist[1][h].toString()+"div").className!="dotEmpty"){
+			brownlist = [[blacklist[0][h]],[blacklist[1][h]]];
+		}
+		var oldLength = 0;
+	  	var newLength = 0;
+		var edge = false;
+	  	do{
+			oldLength = whitelist[0].length;
+		  	for(k=0; k<oldLength; k++){
+				if(checkAdjacent(whitelist[0][k],whitelist[1][k],cl)==true){
+					edge=true;
+					break;
+				}
+		  	}
+			if(edge==true){break;}
+		  	newLength=whitelist[0].length;
+	  	}while(newLength>oldLength);
+		if(edge==false){
+			for(f=0;f<brownlist[0].length;f++){
+				var left = brownlist[0][f].toString() + "," + brownlist[1][f].toString();
+				document.getElementById(left+"div").className=cl;
+				resetCount();
+			}
+		}
+	}
+}
+
+function checkStart(x,y,cl){
 	if(x>0){
 		var xnew = x-1;
 		var ynew = y;
@@ -205,40 +188,98 @@ function checkStart(x, y, cl) {
 		}
 	}
 }
-function checkCapture(id) {
-    "use strict";
-	var idString = id.split(","), x = parseInt(idString[0], 10), y = parseInt(idString[1], 10), cl = document.getElementById(id + "div").className, blacklist = [[], []];
-	checkStart(x, y, cl);
-	for (h = 0; h < blacklist[0].length; h += 1) {	
-		whitelist = [[blacklist[0][h]],[blacklist[1][h]]];
-		brownlist = [[],[]];
-		if(document.getElementById(blacklist[0][h].toString() + "," + blacklist[1][h].toString()+"div").className!="dotEmpty"){
-			brownlist = [[blacklist[0][h]],[blacklist[1][h]]];
-		}
-		var oldLength = 0;
-	  	var newLength = 0;
-		var edge = false;
-	  	do{
-			oldLength = whitelist[0].length;
-		  	for(k=0; k<oldLength; k++){
-				if(checkAdjacent(whitelist[0][k],whitelist[1][k],cl)==true){
-					edge=true;
-					break;
-				}
-		  	}
-			if(edge==true){break;}
-		  	newLength=whitelist[0].length;
-	  	}while(newLength>oldLength);
-		if(edge==false){
-			for(f=0;f<brownlist[0].length;f++){
-				var left = brownlist[0][f].toString() + "," + brownlist[1][f].toString();
-				document.getElementById(left+"div").className=cl;
-				resetCount();
+
+function checkAdjacent(x,y,cl){
+	var edge = false;
+	var adjacents = [0,0,0,0];
+	if(x>0){
+		var xnew = x-1;
+		var ynew = y;
+		var left = xnew.toString() + "," + ynew.toString();
+		if(document.getElementById(left+"div").className!=cl){
+			if(white(xnew,ynew)==false){
+				adjacents[1]=1;
+			}
+			if(document.getElementById(left+"div").className!="dotEmpty"){
+				brown(xnew,ynew);
 			}
 		}
 	}
+	else{edge=true;}
+	if(x<columnCount-1){
+		var xnew = x+1;
+		var ynew = y;
+		var right = xnew.toString() + "," + ynew.toString();
+		if(document.getElementById(right+"div").className!=cl){
+			if(white(xnew,ynew)==false){
+				adjacents[2]=1;
+			}
+			if(document.getElementById(right+"div").className!="dotEmpty"){
+				brown(xnew,ynew);
+			}
+		}
+	}
+	else{edge=true;}
+	if(y>0){
+		var xnew = x;
+		var ynew = y-1;
+		var top = xnew.toString() + "," + ynew.toString();
+		if(document.getElementById(top+"div").className!=cl){
+			if(white(xnew,ynew)==false){
+				adjacents[0]=1;
+			}
+			if(document.getElementById(top+"div").className!="dotEmpty"){
+				brown(xnew,ynew);
+			}
+		}
+	}
+	else{edge=true;}
+	if(y<rowCount-1){
+		var xnew = x;
+		var ynew = y+1;
+		var bottom = xnew.toString() + "," + ynew.toString();
+		if(document.getElementById(bottom+"div").className!=cl){
+			if(white(xnew,ynew)==false){
+				adjacents[3]=1;
+			}
+			if(document.getElementById(bottom+"div").className!="dotEmpty"){
+				brown(xnew,ynew);
+			}
+		}
+	}
+	else{edge=true;}
+	return edge;
 }
-function takeTurn() {
+
+function brown(xnew,ynew){
+	var brownb = false;
+	for(i=0;i<brownlist[0].length;i++){
+		if(brownlist[0][i]==xnew && brownlist[1][i]==ynew){
+			brownb=true;
+		}
+	}
+	if(brownb==false){
+		brownlist[0].push(xnew);
+		brownlist[1].push(ynew);
+	}
+	return brownb;
+}
+
+function white(xnew,ynew){
+	var whiteb = false;
+	for(i=0;i<whitelist[0].length;i++){
+		if(whitelist[0][i]==xnew && whitelist[1][i]==ynew){
+			whiteb=true;
+		}
+	}
+	if(whiteb==false){
+		whitelist[0].push(xnew);
+		whitelist[1].push(ynew);
+	}
+	return whiteb;
+}
+
+function takeTurn(){
 	document.getElementById("p"+turn).style.transform="scale(1,1)";
 	turn = (turn+1)%6;
 	var i = 0;
@@ -253,7 +294,29 @@ function takeTurn() {
 	}
 	document.getElementById("p"+turn).style.transform="scale(1.1,1.1)";
 }
-function playerChange(player) {
+
+function resetCount(){
+	count = [0,0,0,0,0,0];
+	var tds = document.getElementsByTagName("td");
+	var tdCount = 0;
+	for(i=0;i<rowCount;i++){
+		for(j=0;j<columnCount;j++){
+			var tdid = tds[tdCount].id;
+			var cl = document.getElementById(tdid+"div").className;
+			var piece = cl.split("dot")[1];
+			if(piece!="Empty"){
+				var turn = parseInt(piece);
+				count[turn]=count[turn]+1;
+			}
+			tdCount++;
+		}
+	}
+	for(i=0;i<6;i++){
+		document.getElementById("p"+i+"count").innerHTML = count[i];
+	}
+}
+
+function playerChange(player){
 	var play = document.getElementById(player);
 	var playerNumber = parseInt(player.split("p")[1]);
 	var inner = play.innerHTML.split("<");
@@ -275,7 +338,8 @@ function playerChange(player) {
 		takeTurn();
 	}
 }
-function scaleGrid() {
+
+function scaleGrid(){
 	var table = document.getElementById("backgroundTable");
 	var table2 = document.getElementById("visibleTable");
 	var newRows = Math.round(((winHeight-(winWidth*.2)) / gridSize)-1.5);
@@ -288,7 +352,7 @@ function scaleGrid() {
 	var newColumns2 = newColumns-1;
 	var currentRows2 = table2.rows.length;
 	var currentColumns2 = table2.rows[0].cells.length;
-	
+
 	//columns
 	if(newColumns>currentColumns){
 		//add cells to existing rows
@@ -343,7 +407,8 @@ function scaleGrid() {
 	}
 	createIds();
 }
-function createIds() {
+
+function createIds(){
 	var tdCount = 0;
 	var tds = document.getElementsByTagName("td");
 	for(i=0;i<rowCount;i++){
@@ -360,43 +425,3 @@ function createIds() {
 		}
 	}
 }
-function placePiece(id) {
-    "use strict";
-	var name = "dot" + turn;
-	if (document.getElementById(id + "div").className === "dotEmpty") {
-		document.getElementById(id + "div").className = name;
-		count[turn] = count[turn] + 1;
-		document.getElementById("p" + turn + "count").innerHTML = count[turn];
-		checkTaken(id);
-		checkCapture(id);
-		checkLast();
-		takeTurn();
-	}
-}
-
-window.onresize = function (event) {
-	winWidth = Math.max(window.innerWidth, document.documentElement.clientWidth, document.body.clientWidth);
-	winHeight = Math.max(window.innerHeight, document.documentElement.clientHeight, document.body.clientHeight);
-	scaleGrid();
-	resetCount();
-};
-window.onkeypress=function() {
-	var countClass =	document.getElementsByClassName("count");
-	for(i=0;i<countClass.length;i++){
-		if(countClass[i].style.display == "block"){
-			countClass[i].style.display = "none";
-		}
-		else{
-			countClass[i].style.display = "block";
-		}
-	}
-};
-function startup1(e) {
-	winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	scaleGrid();
-	playerChange("p0");
-	playerChange("p1");
-}
-
-addLoadEvent(startup1);
